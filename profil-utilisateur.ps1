828a7c483a0e968184044ee61eff167e8659be12
Neo-code769 <56584577+Neo-code769@users.noreply.github.com>
2025-04-22 08:56:10 +0200

    Update profil-utilisateur.ps1
    Gender choice

--- profil-utilisateur.ps1
+++ profil-utilisateur.ps1
@@ -1,6 +1,19 @@
-﻿$nom= Read-Host "Quel est ton prénom et ton nom"
-$age= [int](Read-Host "Quel est ton âge")
+$nom = Read-Host "Quel est ton prénom et ton nom"
+$age = [int](Read-Host "Quel est ton âge")
+$genre = Read-Host "Quel est ton genre (Homme/Femme/Autre)"
 
-$naissance= (Get-Date).year - $age
+$naissance = (Get-Date).Year - $age
 
-Write-Output "Enchanté $nom tu es née en $naissance"
+switch ($genre.ToLower()) {
+    "femme" {
+        $phrase = "Enchantée $nom, tu es née en $naissance"
+    }
+    "homme" {
+        $phrase = "Enchanté $nom, tu es né en $naissance"
+    }
+    default {
+        $phrase = "Enchanté(e) $nom, tu es né(e) en $naissance"
+    }
+}
+
+Write-Output $phrase
